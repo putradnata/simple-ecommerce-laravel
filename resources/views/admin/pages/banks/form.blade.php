1a6b603b3390ffa37912d54e7a296262fe55c51a
Putra Dinata <putradnata15@gmail.com>
2022-11-07 13:14:17 +0800

    Update form.blade.php
    add one input type for reference

--- resources/views/admin/pages/banks/form.blade.php
+++ resources/views/admin/pages/banks/form.blade.php
@@ -46,7 +46,45 @@
             </div>
         @endif
 
+        {{-- conditional form tag for CREATE and UPDATE --}}
+        {{-- @if (!isset($request->id))
+            <form method="post" action="{{ route('admin-management.store') }}" enctype="multipart/form-data">
+            @else
+                <form method="post" action="{{ route('admin-management.update', $request->id) }}"
+                    enctype="multipart/form-data">
+                    @method('PUT')
+        @endif --}}
 
+        {{-- @csrf --}}
+        <div class="form-group row">
+            <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+            <div class="col-sm-10">
+                {{-- example: Form with validation, with error message underneath the input, if the data was wrong when submitted, value still exists --}}
+
+                {{-- <input type="text" class="form-control @error('name') is-invalid @enderror" id="nama"
+                    placeholder="Nama Admin" name="nama" value="{{ old('nama', $request->name) }}" autocomplete="nama"
+                    required/> --}}
+
+                {{-- error message for input type above --}}
+                {{-- @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror --}}
+
+                <input type="text" class="form-control @error('name') is-invalid @enderror" id="nama"
+                    placeholder="Nama Admin" name="nama" autocomplete="nama" required />
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="col-sm-12">
+                <div class="d-flex justify-content-end">
+                    <input type="submit" value="Submit" class="btn btn-primary justify-content-end" />
+                </div>
+            </div>
+        </div>
+        {{-- </form> --}}
 
     @endsection
 
